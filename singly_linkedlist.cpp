8c411770d91ae915de0e0292530012e2ff4adf89
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-08 20:12:14 +0700

    Switch case 4

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -155,6 +155,29 @@ int main()
                 mhs.traverse();
             }
             break;
+        case '4':
+            {
+                if(mhs.listEmpty() == true)
+                    {
+                        cout << "\nList Kosong\n";
+                        break;
+                    }
+                Node *previous, *current;
+                cout << endl
+                        << "Masukkan nomor mahasiswa yang dicari : ";
+                cin >> nim;
+                if (mhs.Search(nim, &previous, &current) == false)
+                    cout << endl
+                            << "Data tidak ditemukan" << endl;
+                else
+                {
+                    cout << endl
+                            << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                    cout << "\n";
+                }
+            }
+            break;
         }
     } while (ch != '5');
     
