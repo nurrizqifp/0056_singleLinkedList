3735cfc435f6ceb251d75296b43713eb013fc0e7
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-08 20:00:48 +0700

    Switch case 1

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -123,6 +123,16 @@ int main()
         cout << endl
             << "Masukkan Pilihan (1-5):";
         cin >> ch;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            mhs.addNode();
+            break;
+        }
+        
+        }
     } while (ch != '5');
     
 }
